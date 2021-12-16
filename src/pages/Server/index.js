a54c8a93b9412d3f1cf3fcf0aef198d30d1bf080
PawelOleniak <pawel.oleniak00@wp.pl>
2021-12-16 10:53:47 +0100

    port tweak

--- src/pages/Server/index.js
+++ src/pages/Server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql');
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 80;
 
 const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
