584503302c7bafb1fe4ca19760910a41ee6c28b1
PawelOleniak <pawel.oleniak00@wp.pl>
2022-03-24 22:44:10 +0100

    heroku deploynment tweak

--- src/Server.js
+++ src/Server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 const pool = mysql.createPool({
   host: 'us-cdbr-east-05.cleardb.net',
   user: 'b6731f58f87274',
